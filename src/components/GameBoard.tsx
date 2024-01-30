4387f72f686877f969c22bbcddba5866d204039f
Tom Spencer <tom.spencer@studiozeffa.com>
2024-01-30 21:15:11 +0000

    Relocate logic

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -11,9 +11,12 @@ const initialCells = [
 ];
 const initialTurnDeque = ['O', 'X'];
 
+type Cells = typeof initialCells;
+type TurnDeque = typeof initialTurnDeque;
+
 const GameBoard = () => {
-  const [cells, setCells] = useState(structuredClone(initialCells));
-  const [turnDeque, setTurnDeque] = useState([...initialTurnDeque]);
+  const [cells, setCells] = useState<Cells>(structuredClone(initialCells));
+  const [turnDeque, setTurnDeque] = useState<TurnDeque>([...initialTurnDeque]);
 
   const handleCellPress = (rowIdx: number, colIdx: number) => {
     if (cells[rowIdx][colIdx] !== '') {
@@ -34,50 +37,18 @@ const GameBoard = () => {
   };
 
   const nextGame = () => {
+    // Reset the state
     setCells(structuredClone(initialCells));
     setTurnDeque([...initialTurnDeque]);
   };
 
-  const isBoardFull = () => {
-    for (let row = 0; row < 3; row++) {
-      for (let col = 0; col < 3; col++) {
-        if (cells[row][col] === '') {
-          return false; // Found an empty cell, so the cells is not full
-        }
-      }
-    }
-    return true; // No empty cells found, the cells is full
-  };
-
-  const getWinner = () => {
-    // Check rows and columns
-    for (let i = 0; i < 3; i++) {
-      if (cells[i][0] === cells[i][1] && cells[i][1] === cells[i][2] && cells[i][0] !== '') {
-        return cells[i][0];
-      }
-      if (cells[0][i] === cells[1][i] && cells[1][i] === cells[2][i] && cells[0][i] !== '') {
-        return cells[0][i];
-      }
-    }
-
-    // Check diagonals
-    if (cells[0][0] === cells[1][1] && cells[1][1] === cells[2][2] && cells[0][0] !== '') {
-      return cells[0][0];
-    }
-    if (cells[0][2] === cells[1][1] && cells[1][1] === cells[2][0] && cells[0][2] !== '') {
-      return cells[0][2];
-    }
-
-    return null;
-  };
-
   const gameStatus = (() => {
-    const winner = getWinner();
+    const winner = getWinner(cells);
     if (winner !== null) {
       return `${winner} wins!`;
     }
 
-    const gameOver = isBoardFull();
+    const gameOver = isBoardFull(cells);
     if (gameOver) {
       return 'Game over!';
     }
@@ -85,7 +56,7 @@ const GameBoard = () => {
     return `Next turn: ${turnDeque[0]}`;
   })();
 
-  const isGameOver = getWinner() !== null || isBoardFull();
+  const isGameOver = getWinner(cells) !== null || isBoardFull(cells);
 
   return (
     <Box gap="s">
@@ -130,3 +101,37 @@ const GameBoard = () => {
 };
 
 export default GameBoard;
+
+function isBoardFull(cells: Cells) {
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 3; col++) {
+      if (cells[row][col] === '') {
+        return false; // Found an empty cell, so the cells is not full
+      }
+    }
+  }
+  return true; // No empty cells found, the cells is full
+}
+
+function getWinner(cells: Cells) {
+  for (let i = 0; i < 3; i++) {
+    if (cells[i][0] === cells[i][1] && cells[i][1] === cells[i][2] && cells[i][0] !== '') {
+      // Every cell on this row is the same: winner!
+      return cells[i][0];
+    }
+    if (cells[0][i] === cells[1][i] && cells[1][i] === cells[2][i] && cells[0][i] !== '') {
+      // Every cell on this column is the same: winner!
+      return cells[0][i];
+    }
+  }
+
+  // Check diagonals
+  if (cells[0][0] === cells[1][1] && cells[1][1] === cells[2][2] && cells[0][0] !== '') {
+    return cells[0][0];
+  }
+  if (cells[0][2] === cells[1][1] && cells[1][1] === cells[2][0] && cells[0][2] !== '') {
+    return cells[0][2];
+  }
+
+  return null;
+}
